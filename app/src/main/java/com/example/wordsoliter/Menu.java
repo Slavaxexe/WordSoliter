f24971af5349ab839f5efc0d1bea3ed3e8cd523b
slava <mister.slava1508@yandex.ru>
2022-05-23 00:07:59 +0300

    menu created

---
+++ app/src/main/java/com/example/wordsoliter/Menu.java
@@ -0,0 +1,44 @@
+package com.example.wordsoliter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+import android.view.MenuInflater;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        BottomNavigationView bnv=  findViewById(R.id.bottomNavigationView);
+        Set<Integer> a = new HashSet<>();
+        a.add(R.id.start); a.add(R.id.shop); a.add(R.id.statistics);
+        NavHostFragment navHostFragment =
+                (NavHostFragment) fragmentManager.findFragmentById(R.id.fragmentContainerView);
+        NavController navController = navHostFragment.getNavController();
+        AppBarConfiguration barConfiguration = new AppBarConfiguration.Builder(a).build();
+        NavigationUI.setupWithNavController(bnv, navController);
+        NavigationUI.setupActionBarWithNavController(this, navController, barConfiguration);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(android.view.Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+}
