2f522625eda2803b68a5fe2d763f3d48fcd76323
slava <mister.slava1508@yandex.ru>
2022-05-27 02:53:05 +0300

    splash screen added

---
+++ app/src/main/java/com/example/wordsoliter/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.wordsoliter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        getSupportActionBar().hide();
+        Handler handler = new Handler();
+        final Runnable r = new Runnable() {
+            public void run() {
+                start();
+            }
+        };
+        handler.postDelayed(r, 2000);
+    }
+    void start(){
+        Intent intent = new Intent(this, Menu.class);
+        startActivity(intent);
+        finish();
+    }
+}
